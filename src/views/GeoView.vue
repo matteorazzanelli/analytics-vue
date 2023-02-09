cfdeac5f87dff5ab957f7733b67271ba48d9c435
matteo <matteo.razzanelli89@gmail.com>
2023-02-09 09:53:06 +0100

    GeoView view set for including maps as components

--- src/views/GeoView.vue
+++ src/views/GeoView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="geo">
+    <h1>Geographical informations</h1>
+    <h3>Where the most traffic comes from</h3>
+    <Map></Map>
   </div>
 </template>
+
+
+<script>
+import Map from '@/components/Map.vue'
+
+export default {
+  name: 'GeoView',
+  components: { Map },
+  setup(){ }
+}
+</script>
+
+<style scoped>
+.geo{
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+}
+</style>
