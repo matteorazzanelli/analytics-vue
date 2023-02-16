1915388773a7dc4b97e4e2e07506efcfcba0a6ac
matteo <matteo.razzanelli89@gmail.com>
2023-02-16 17:33:58 +0100

    graphs are now reactive with respect to changing variables

--- src/stores/StatsStore.js
+++ src/stores/StatsStore.js
@@ -6,18 +6,24 @@ export const useStatsStore = defineStore('statsStore', () => {
   // data
   const stats = ref({});
   const loaded = ref(false);
+  const date = new Date();
+  let currentDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
   // methods
   function setValues(obj){
+    
     for (const key of Object.keys(obj)){
       stats.value[key] = {
         'history': obj[key]['history'],
         'total': obj[key]['total']
       }
-    }
+      // add today as a new data field
+      stats.value[key]['history'][currentDate] = 0
+    }    
   }
 
   function increment(kpi, value){
+    stats.value[kpi]['history'][currentDate] += value
     stats.value[kpi]['total'] += value;
   }
 
